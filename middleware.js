246aed94678e61177aec681cad04cd3ea994ec77
uni <7pluto8@gmail.com>
2025-12-10 22:32:21 +0900

    add middleware for vercel and setting

---
+++ middleware.js
@@ -0,0 +1,30 @@
+import { next } from "@vercel/edge";
+
+// demoページ以下はbasic認証
+export const config = {
+  matcher: ["/demo/:path*"],
+};
+
+export default function middleware(request) {
+  const authorizationHeader = request.headers.get("authorization");
+
+  // 正解のユーザー/パスワード
+  const validUser = process.env.BASIC_AUTH_USER;
+  const validPass = process.env.BASIC_AUTH_PASSWORD;
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(" ")[1];
+    const [user, password] = atob(basicAuth).split(":");
+
+    if (user === validUser && password === validPass) {
+      return next();
+    }
+  }
+
+  return new Response("Auth Required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
+  });
+}
